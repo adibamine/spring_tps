076554ab06a736649835b8f8cf897c25439893bd
adibamine <adibamine1@gmail.com>
2022-03-01 23:40:04 +0100

    actuator completed

---
+++ src/main/java/ma/octo/training/HealthCheck.java
@@ -0,0 +1,23 @@
+package ma.octo.training;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component("myHealthCheck")
+public class HealthCheck implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        int errorCode = check();
+        if (errorCode != 0) {
+            return Health.down()
+                    .withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    public int check() {
+        return (int) Math.round(Math.random());
+    }
+}
