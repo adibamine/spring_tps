45d682d60161a79ea6dee114792dcffd90a2a0be
adibamine <adibamine1@gmail.com>
2022-03-02 21:56:09 +0100

    profiles config terminated

--- src/main/java/ma/octo/training/StartupRunner.java
+++ src/main/java/ma/octo/training/StartupRunner.java
@@ -2,6 +2,7 @@ package ma.octo.training;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -11,8 +12,15 @@ public class StartupRunner implements ApplicationRunner {
 
     private static final Logger log = LoggerFactory.getLogger(StartupRunner.class);
 
+    @Value("${database.url}")
+    private String dbUrl;
+    @Value("${cache.ttl:60}")
+    private Integer cacheTtl;
+
     @Override
     public void run(ApplicationArguments args) {
         log.info("Hello World");
+        log.info("database URL : {}", dbUrl);
+        log.info("cache TTL : {}", cacheTtl);
     }
 }
